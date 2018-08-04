3db8dab34ccfb22a25b1f9698c66b1fc2889906c
jsm <joshuamaza209@gmail.com>
2018-08-04 11:07:40 -0700

    working solution for enRouteSalute

--- Level 2/En Route Salute/enRouteSalute.java
+++ Level 2/En Route Salute/enRouteSalute.java
@@ -28,6 +28,7 @@ and returns the number of times the employees will salute. s will contain at lea
 
  */
 
+// INITIAL THOUGHT PROCESS:
 // Take the string, and delete all the "-"
 // For loop over the length of the string.
 // The equation is "<" added up, then times two for every ">" counted.
@@ -35,55 +36,44 @@ and returns the number of times the employees will salute. s will contain at lea
 // Front case of starting line with "<", since they will never salute anyone, so ignore them.
 // If you run into "<", increment a counter.
 // If you run into ">" increment a counter.
-// If you run into "<" again, stop and add to the total salute count by doing above calculation, then set "<" counter
+// If you run into "<" again, stop and add to the total salute count by doing above calculation, 
+// then set "<" counter
 // to zero, but keep ">" counter the same.
 // <--<-->-->--<--<-->---->---<--->---<
-// Actually retrospect this doesn't work when loop encounters ">" then "<", ending after one sign change.
+// Actually retrospect this doesn't work when loop encounters ">" then "<", ending after one sign 
+// change.
+// Now deciding to take simple route to actually finish the program, by simply counting the amount
+// of salutes per worker facing a certain direction... I will start from the left side.
+
+
+// enRouteSalute will count the amount of salutes between workers, represented by the direction
+// they're walking, or "<" and ">". It is assumed that they all travel at the same speed at the
+// same time, with empty spaces in the hallway represented by "-". A salute occurs when two
+// workers have to cross paths, where they both will do one salute each.
 
 public class enRouteSalute {
-
-    public static void main(String[] args) {
-        System.out.println(totalSalutes("<--<-->-->--<--<-->---->---<--->---<"));
-        System.out.println(totalSalutes("-<->><-->>-<<->->"));
-
-
-
-    }
-
-    public static String format(String s) {
-        s = s.replace("-", "");
-        int i = 0;
-        while (s.charAt(i) == '<') {
-            i++;
-        }
-        return s.substring(i);
-    }
-
-    public static int calculateSalutes(int right, int left) {
-        return right * left * 2;
-    }
-
-    public static int totalSalutes(String saluteLine) {
-        saluteLine = format(saluteLine);
-        int right = 0;
-        int left = 0;
-        int totalSalutes = 0;
-        boolean encounteredLeft = false;
-        for (int i = 0; i < saluteLine.length(); i++) {
-            if (saluteLine.charAt(i) == '>') {
-                right++;
-                if (encounteredLeft) {
-                    totalSalutes += calculateSalutes(right, left);
-                    left = 0;
-                    encounteredLeft = false;
-                }
-            } else { // ran into <
-                left++;
-                encounteredLeft = true;
+   
+   public static void main(String[] args) {
+      System.out.println(totalSalutes("<--<-->-->--<--<-->---->---<--->---<"));
+      System.out.println(totalSalutes("-<->><-->>-<<->->"));
+      System.out.println(totalSalutes("-<-><-->-"));
+      System.out.println(totalSalutes("--->-><-><-->-"));
+   }
+   
+   // Pre: Given a String "saluteLine" that contains only the characters "<, >, -"
+   // Post: Will return an integer representing the amount of salutes, which occurs whenever
+   // two opposing arrows will cross each other.
+   public static int totalSalutes(String saluteLine) {
+      int count = 0;
+      for (int i = 0; i < saluteLine.length(); i++) {
+         if (saluteLine.charAt(i) == '>') {
+            for (int j = i; j < saluteLine.length(); j++) {
+               if (saluteLine.charAt(j) == '<') {
+                  count += 1;
+               }
             }
-
-        }
-        //totalSalutes += calculateSalutes(right, left);
-        return totalSalutes;
-    }
+         }     
+      }
+      return count;
+   }
 }
